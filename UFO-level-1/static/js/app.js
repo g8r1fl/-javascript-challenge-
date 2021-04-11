70046413e9d634910c510e80757565a14e96feee
Howard <g8r1fl@gmail.com>
2021-04-10 23:27:01 -0400

    trying to figure out how to query the array by using filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,30 +11,37 @@ tableData.forEach(function (item) {
   console.log(item["datetime"]);
 });
 
-// next, let's build our js filter(s).
-let date = "1/1/2010";
+// // next, let's build our js filter(s).
+let date = "az";
+function dateCheck(date) {
+  return date.state === date;
+}
 
-// filter right? You only want elts with that date.
-tableData.filter((datum) => {
-  // optional: check if the datetime key exists
-  // return datum.datetime === uFillThisIn // this is the guy u need to look at
-});
+var testDate = tableData.filter(dateCheck);
 
-// YOUR CODE HERE!
-let filteredData = tableData.slice(0, 10);
+console.log(testDate);
+// // filter right? You only want elts with that date.
+// tableData.filter((datum) => {
+//   // optional: check if the datetime key exists
+//   return datum.datetime === date;    //uFillThisIn // this is the guy u need to look at
+//   console.log(datum.datetime === date);
+// });
 
-let table = d3.select("tbody");
+// // YOUR CODE HERE!
+// let filteredData = tableData.slice(0, 10);
 
-tableData.forEach((rec) => {
-  let tr = table.append("tr");
-  tr.append("td").text(rec.datetime);
-  tr.append("td").text(rec.state);
-});
+// let table = d3.select("tbody");
 
-d3.select("button").on("click", () =>
-  filteredData.forEach((rec) => {
-    let tr = table.html("").append("tr");
-    tr.append("td").text(rec.datetime);
-    tr.append("td").text(rec.city);
-  })
-);
+// tableData.forEach((rec) => {
+//   let tr = table.append("tr");
+//   tr.append("td").text(rec.datetime);
+//   tr.append("td").text(rec.state);
+// });
+
+// d3.select("button").on("click", () =>
+//   filteredData.forEach((rec) => {
+//     let tr = table.html("").append("tr");
+//     tr.append("td").text(rec.datetime);
+//     tr.append("td").text(rec.city);
+//   })
+// );
