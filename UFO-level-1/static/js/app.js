9b49d034172f4305c88137279a4d9ef5ce6a4a73
Howard <g8r1fl@gmail.com>
2021-04-16 17:13:37 -0400

    finalized level 1 of the homework.  Will submit and try bonus later

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,145 +1,53 @@
 
 // YOUR CODE HERE!
 // from data.js
-var tableData = data;
+var tableData = data; //why const vs var or let?
 
-console.log("tableData", tableData);
+//get table reference
+const table = d3.select("tbody");//why const vs var or let?
 
-// for now, hardcode a datetime
-
-// tableData.forEach(function (item) {
-//   console.log(item["datetime"]);
-// });
-
-
-
-// // next, let's build our js filter(s).
-// let date = "1/7/2010";
-// function dateCheck(arr) {
-//   arr.forEach(item => {
-//     if (arr.datetime === date) {
-//       return arr.datetime;
-//     }
-//   })
-// }
-// var checkDate = tableData.filter(dateCheck);
-// console.log(checkDate);
-// var testDate = tableData.filter(dateCheck);
-// date = "1/1/2020";
-// var date = d3.select("#datetime").property("value");
-// function dateCheck(arr) {
-//   return arr.datetime === date;
-// }
-// // var testDate = tableData.filter(dateCheck)
-// // console.log(testDate);
-
-// var state = "ny";
-// function stateCheck(arr) {
-//   return arr.state === state;
-// }
-// var testState = tableData.filter(stateCheck);
-// console.log(testState);
-
-// var arrCombo = tableData.filter(rec => {
-//   return rec.datetime === date;}).filter(rec => {
-//     return rec.state === state;});
-// console.log("This is my new date and state filter", arrCombo);
-// // filter right? You only want elts with that date.
-// tableData.filter((datum) => {
-//   // optional: check if the datetime key exists
-//   return datum.datetime === date;    //uFillThisIn // this is the guy u need to look at
-//   console.log(datum.datetime === date);
-// });
-
-// // // YOUR CODE HERE!
-// //build the table on html
-// let filtered = tableData.filter(dateCheck);
-
-const table = d3.select("tbody");
-
-// tbody.html("");
 //loop through filtered data rows to build table rows
 function buildTable(arr) {
   // clear tables if any
   table.html("");
-  
+  // loop through array and insert table row
   arr.forEach(row => {
-    const tr = table.append("tr");
+    const tr = table.append("tr");//why const vs var or let?
     // var rowData = Object.values(row)
+    // loop through elements in each row and add table data
     Object.values(row).forEach(rec => {
       tr.append("td").text(rec);
-    });
-    console.log("Table built!");
-  })
+      }
+    );
+    
+  });
 
 }
 
-
-// var button = d3.select("button");
-// var inputElement = d3.select("#datetime");
-// var inputValue = inputElement.property("value");
+// create function to handle click
 
 function handleClick() {
-  console.log("A button was clicked!");
-  const date = d3.select("#datetime").property("value");
-  console.log(date);
+  // grab the date input in the form
+  const date = d3.select("#datetime").property("value");//why const vs var or let?
+  // refer to the original array
   let filtered = tableData;
-  
+  // check if input date exists in array
   if (date) {
-    filtered = filtered.filter(row => row.datetime === date)
+    filtered = filtered.filter(row => row.datetime === date);
   }
+  // build filtered table
   buildTable(filtered);
-  console.log("Filtered table built!");
+  
 }
 
+// handle click 
+d3.select("button").on("click", handleClick);
+
+// preload table
 buildTable(tableData);
 
-d3.select("button").on("click", handleClick);
-// d3.select("button").on("click", () =>
-//   // filteredData.forEach((rec) => {
-//   //   let tr = table.html("").append("tr");
-//   //   tr.append("td").text(rec.datetime);
-//   //   tr.append("td").text(rec.city);
-//   // })
-// );
-// filtered.forEach(row => {
-//   // clear out any tables existing
-  
-//   // Object.value(row).forEach(rec => {
-//   //   console.log(rec.datetime);
-//   // })
-//   console.log(row.datetime);
-//   console.log(row.city);
-//   console.log(row.state);
 
 
-//   //loop through each element in the row and add the data
-//   //add a row
-//   // const tr = table.append("tr");
-  
-//   // Object.value(row).forEach(rec => {
-//   //   console.log(rec);
-//   // })
-// });
-
-
-// let filteredData = tableData.slice(0, 10);
-
-// let table = d3.select("tbody");
-
-// tableData.forEach((rec) => {
-//   let tr = table.append("tr");
-//   tr.append("td").text(rec.datetime);
-//   tr.append("td").text(rec.state);
-// });
-
-// d3.select("button").on("click", () =>
-//   filteredData.forEach((rec) => {
-//     let tr = table.html("").append("tr");
-//     tr.append("td").text(rec.datetime);
-//     tr.append("td").text(rec.city);
-//   })
-// );
 
 
 // for bonus
